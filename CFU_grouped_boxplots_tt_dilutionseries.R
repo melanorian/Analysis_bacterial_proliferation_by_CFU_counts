5fcd426083a3405b168f639cdf5915c6de18068f
Melanie <m.n.mendel@uu.nl>
2024-10-16 13:56:44 +0200

    Produced output for CFU different spinach lines. Messy version...accounting for additonal level of variables (Spinach lines)

--- CFU_grouped_boxplots_tt_dilutionseries.R
+++ CFU_grouped_boxplots_tt_dilutionseries.R
@@ -12,9 +12,9 @@ rm(list=ls())
 library(ggplot2)
 library(dplyr)
 library(multcompView)
-library(agricolae)
+install.packages("agricolae")
 library(ggsignif)
-library(ggpubr)
+library("ggpubr")
 library("report")
 library("readxl")
 
@@ -22,14 +22,14 @@ library("readxl")
 variance_for_tt <- TRUE
 SUF <- "_rm0_nomin_nooutlier"
 setwd("/home/melanie/D36E_assays/CFU_data/")
-dir_out <- "/home/melanie/D36E_assays/CFU_output/2025092024/t0_t2/"
+dir_out <- "/home/melanie/D36E_assays/CFU_output/20240513/"
 
 # generate prefix string including date/ Initials
 current_date <- gsub("-", "", as.character(Sys.Date()))# Get the current date as character
 pre <- paste("MM", current_date, sep = "")
 
 # filter for samples of interest 
-pst_raw <- read_xlsx("2025092024_CFU_count_calculations_dilutions.xlsx")
+pst_raw <- read_xlsx("MM20240513_summary_CFU_count_calculations_cultivars.xlsx")
 
 # 2. Select/ filter data ----
 # 2.1 Pseudomonas strains as factors
@@ -42,26 +42,29 @@ pst_raw$Date <- as.factor(pst_raw$dpi)
 zeros <- pst_raw[!pst_raw$`Calculate CFU/ml per 1 cm2 of harvested leaf` == 0 ,]
 cat("nr removed rows due to zero-value",nrow(pst_raw) - nrow(zeros))
 pst_raw <- zeros
+rm(zeros)
 
 # 2.2 log10 transformation of data for better presentation
-MIN <- min(pst_raw$`Calculate CFU/ml per 1 cm2 of harvested leaf`[pst_raw$`Calculate CFU/ml per 1 cm2 of harvested leaf`!=0]) # set lowest CFU counts as 0
-pst_raw$CFU.log10 <- log10(pst_raw$`Calculate CFU/ml per 1 cm2 of harvested leaf`) #+MIN)
+#MIN <- min(pst_raw$`Calculate CFU/ml per 1 cm2 of harvested leaf`[pst_raw$`Calculate CFU/ml per 1 cm2 of harvested leaf`!=0]) # set lowest CFU counts as 0
+pst_raw <- pst_raw[!is.na(as.numeric(pst_raw$`Calculate CFU/ml per 1 cm2 of harvested leaf`)), ] # remove rows with na
+pst_raw$CFU.log10 <- log10(as.numeric(pst_raw$`Calculate CFU/ml per 1 cm2 of harvested leaf`)) #+MIN)
 
 # 2.3 make df containing only data I want to plot: Pst strain, CFU log10 transormed
 Pst_strain <- as.factor(pst_raw$Pseudomonas.strain)
 Pst_CFU <- pst_raw$CFU.log10
 Pst_exp_date <- as.factor(pst_raw$Date)
-Pst_OD <- pst_raw$`OD6000 infiltrated 0dpi`
+Pst_OD <- pst_raw$`Code of other samples on leaf`
 Pst_comment <- pst_raw$Notes
 
-simple_df <- data.frame(Pst_strain, Pst_OD, Pst_exp_date, Pst_CFU, Pst_comment)
+simple_df <- data.frame(Pst_strain, Pst_OD, Pst_exp_date, Pst_CFU)
 
 # 2.5 filtre for infiltrated samples
 simple_df_un <- simple_df[simple_df$Pst_exp_date == "i", ] # infiltrate samples
 simple_df <- simple_df[!(simple_df$Pst_exp_date == "i"), ] # in planta samples
 
-# Remove 6 dpi samples
-simple_df <- simple_df[!(simple_df$Pst_exp_date == "6"), ]
+# Remove 6 dpi samples, "bad" samples 
+#simple_df <- simple_df[!(simple_df$Pst_exp_date == "6"), ]
+simple_df <- simple_df[!(simple_df$Pst_comment == "bad"), ]
 
 # Remove outliers
 remove_outliers <- function(df) {
@@ -69,7 +72,7 @@ df %>%
 group_by(Pst_strain, Pst_OD, Pst_exp_date) %>%
 
 filter(Pst_CFU >= (quantile(Pst_CFU, 0.25) - 1.5 * IQR(Pst_CFU)) &
-     Pst_CFU <= (quantile(Pst_CFU, 0.75) + 1.5 * IQR(Pst_CFU)))
+ Pst_CFU <= (quantile(Pst_CFU, 0.75) + 1.5 * IQR(Pst_CFU)))
 }
 
 # Apply the function
@@ -102,21 +105,23 @@ return(g)
 
 # Generate the plots using lapply
 co <- c("#44AA99", "#E69F00", "#999999")
+
+simple_df <- simple_df[(simple_df$Pst_strain == "D36" | simple_df$Pst_strain == "DC3000"),]
 OD_plot_list <- lapply(unique_OD, generate_OD_plot, df = simple_df)
 OD_grid <- ggarrange(plotlist = OD_plot_list, ncol = length(unique_OD), nrow = 1)
 
-# Save the plots using purrr::map2 and a lambda function
-map2(.x = OD_plot_list, 
-.y = unique_OD, 
-.f = ~ggsave(filename = paste0(dir_out, pre, "_boxplot_", SUF, .y, ".svg"),
-        plot = .x, 
-        width = 3.5, 
-        height = 4, 
-        units = "in", 
-        dpi = 300)
-)
+# # Save the plots using purrr::map2 and a lambda function
+# map2(.x = OD_plot_list, 
+# .y = unique_OD, 
+# .f = ~ggsave(filename = paste0(dir_out, pre, "_boxplot_", SUF, .y, ".svg"),
+#       plot = .x, 
+#       width = 3.5, 
+#       height = 4, 
+#       units = "in", 
+#       dpi = 300)
+# )
 
-ggsave(filename = paste0(dir_out, pre, "_OD_grid", SUF, ".svg"),
+ggsave(filename = paste0(dir_out, pre, "_Lines_grid", SUF, ".svg"),
 plot = OD_grid, 
 width = 12, 
 height = 4, 
@@ -153,16 +158,41 @@ return(g)
 strain_plot_list <- lapply(unique_strains, generate_strain_plot, df = simple_df)
 strain_grid <- ggarrange(plotlist = strain_plot_list, ncol = length(unique_strains), nrow = 1)
 
-# Save the plots using purrr::map2 and a lambda function
-map2(.x = strain_plot_list, 
-.y = unique_strains, 
-.f = ~ggsave(filename = paste0(dir_out, pre, "_boxplot_", SUF, .y, ".svg"),
-        plot = .x, 
-        width = 3, 
-        height = 4, 
-        units = "in", 
-        dpi = 300)
-)
+
+#################
+
+generate_line_plot <- function(df, strain) {
+  subset_df <- df[df$Pst_strain == strain, ]
+  
+  g <- ggplot(subset_df, aes(x = as.factor(Pst_OD), y = Pst_CFU, color = Pst_exp_date)) +
+    geom_boxplot(outlier.colour = "red", alpha = 0.9) +
+    labs(x = strain, y = "log10 (CFU/cm2)") +
+    ggtitle("Strain:", strain) +
+    geom_point(position = position_jitterdodge(0.1)) +
+    theme_classic() +
+  guides(x = guide_axis(angle = 45)) +
+    scale_fill_manual(values = co) +
+    scale_color_manual(values = co) +
+    ylim(2.5, 9)
+  
+  return(g)
+}
+
+# Generate the plots using lapply
+strain_plot_list <- lapply(unique_strains, generate_strain_plot, df = simple_df)
+strain_grid <- ggarrange(plotlist = strain_plot_list, ncol = length(unique_strains), nrow = 1)
+
+
+# # Save the plots using purrr::map2 and a lambda function
+# map2(.x = strain_plot_list, 
+# .y = unique_strains, 
+# .f = ~ggsave(filename = paste0(dir_out, pre, "_boxplot_", SUF, .y, ".svg"),
+#       plot = .x, 
+#       width = 3, 
+#       height = 4, 
+#       units = "in", 
+#       dpi = 300)
+# )
 
 ggsave(filename = paste0(dir_out, pre, "_strain_grid", SUF, ".svg"),
 plot = strain_grid, 
@@ -177,7 +207,7 @@ dpi = 300)
 # 5.1 DEFINE VARIABLES OF INTEREST
 OD <- unique(simple_df$Pst_OD) # extract the ODs from the orginal df
 d1 <- "0"  # Defines the reference dpi (usally 0dpi), epxeriment_date column as input  
-d2 <- "2"  # Defines dpi of interest, usually 2dpi, epxeriment_date column as input
+d2 <- "3"  # Defines dpi of interest, usually 2dpi, epxeriment_date column as input
 
 # 5.2 Define the t-test function
 f_tt <- function(df_Pst, OD, date1, date2, variance_equal) {
@@ -191,13 +221,15 @@ f_perform_tt <- function(subset_list, date1, date2, variance_equal) {
 
 # 1. Perform t-tests on each subset using lapply
 result_list <- lapply(subset_list, function(subset) {
+
+
 lapply(subset, function(df) {
 pval <- f_tt(df_Pst = df,
-         OD = df$Pst_OD[1], # start from first element
-         date1 = date1, 
-         date2 = date2, 
-         variance_equal = variance_equal
-         )
+ #OD = df$Pst_OD[1], # start from first element
+ date1 = date1, 
+ date2 = date2, 
+ variance_equal = variance_equal
+ )
 data.frame(strain = df$Pst_strain[1], dpi = df$Pst_OD[1], p_value = pval)
 })
 })
@@ -227,14 +259,24 @@ sink()
 return(cat(output_file))
 }
 
-# 5.3 Create a list of subsets using lapply and split
+unique_strains <- c("D36", "DC3000")
 subset_list <- lapply(unique_strains, function(strain) {
-subset_df <- simple_df[simple_df$Pst_strain == strain, ]
-split(subset_df, subset_df$Pst_OD)
+  subset_df <- simple_df[simple_df$Pst_strain == strain, ]
+  split(subset_df, list(subset_df$Pst_OD, subset_df$Pst_strain))
 })
 
-res_tt <- f_perform_tt(subset_list = subset_list, 
-   date1 = d1, 
-   date2 = d2, 
-   variance_equal = variance_for_tt
-   )
+cleaned_subset_list <- lapply(subset_list, function(sublist) {
+  Filter(function(df) nrow(df) > 0, sublist)
+})
+# # 5.3 Create a list of subsets using lapply and split
+# subset_list <- lapply(unique_strains, function(strain) {
+# subset_df <- simple_df[simple_df$Pst_strain == strain, ]
+# split(subset_df, subset_df$Pst_OD)
+# })
+
+res_tt <- f_perform_tt(subset_list = cleaned_subset_list, 
+date1 = d1, 
+date2 = d2, 
+variance_equal = variance_for_tt
+)
+
